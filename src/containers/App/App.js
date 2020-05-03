daec925ff5532a512b57647fa5cbfb73e934f8dd
Aysen Armagan <aysen.armagan@gmail.com>
2020-05-03 00:41:53 -0400

    deployed

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
-import 'firebase/firestore' // make sure you add this for firestore
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
-import { createFirestoreInstance } from 'redux-firestore'
-import { Provider } from 'react-redux'
-import ThemeSettings from 'theme'
-import { firebase as fbConfig, reduxFirebase as rfConfig } from 'config'
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore"; // make sure you add this for firestore
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import { Provider } from "react-redux";
+import ThemeSettings from "theme";
+import { firebase as fbConfig, reduxFirebase as rfConfig } from "config";
 
-const theme = createMuiTheme(ThemeSettings)
+const theme = createMuiTheme(ThemeSettings);
 
 // Initialize Firebase instance
-firebase.initializeApp(fbConfig)
+firebase.initializeApp(fbConfig);
 
 function App({ routes, store }) {
   return (
@@ -25,17 +25,18 @@ function App({ routes, store }) {
           firebase={firebase}
           config={rfConfig}
           dispatch={store.dispatch}
-          createFirestoreInstance={createFirestoreInstance}>
+          createFirestoreInstance={createFirestoreInstance}
+        >
           <Router>{routes}</Router>
         </ReactReduxFirebaseProvider>
       </Provider>
     </MuiThemeProvider>
-  )
+  );
 }
 
 App.propTypes = {
   routes: PropTypes.object.isRequired,
-  store: PropTypes.object.isRequired
-}
+  store: PropTypes.object.isRequired,
+};
 
-export default App
+export default App;
